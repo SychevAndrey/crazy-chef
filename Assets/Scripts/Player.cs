bba5ee19c1c5b83c083a858be6bad5cfd74f169d
Andrey Sychev <js@asychev.me>
2024-03-10 20:47:50 +0400

    add interaction with counters

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
     [SerializeField] private float speed = 8f;
     [SerializeField] private GameInput gameInput;
+    [SerializeField] private LayerMask countersLayerMask;
 
     private bool isWalking;
+    private Vector3 lastInteractionDir;
     private void Update() {
+        HandleMovement();
+        HandleInteractions();
+    }
+
+    public bool IsWalking() {
+        return isWalking;
+    }
+
+    private void HandleInteractions() {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 movementDirection = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        if (movementDirection != Vector3.zero) {
+            lastInteractionDir = movementDirection;
+        }
+
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, lastInteractionDir, out RaycastHit raycastHit ,interactDistance, countersLayerMask)) {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)) {
+                clearCounter.Interact();
+            }
+        }
+    }
+
+    private void HandleMovement() {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 movementDirection = new Vector3(inputVector.x, 0f, inputVector.y);
 
@@ -19,7 +47,7 @@ public class Player : MonoBehaviour {
         if (!canMove) {
             Vector3 moveDirX = new Vector3(movementDirection.x, 0f, 0f).normalized;
             canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
-        
+
             if (canMove) {
                 // Can move only in x direction
                 movementDirection = moveDirX;
@@ -45,9 +73,5 @@ public class Player : MonoBehaviour {
 
         float rotateSpeed = 10f;
         transform.forward = (Vector3.Slerp(transform.forward, movementDirection, Time.deltaTime * rotateSpeed));
-    }
-
-    public bool IsWalking() {
-        return isWalking;
     }
 }
