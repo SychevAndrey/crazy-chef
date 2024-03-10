e8fb044acf1d3b3f8d14aec023be1fe5e8406396
Andrey Sychev <js@asychev.me>
2024-03-10 08:15:28 +0400

    add walk and idle animations and cinemachine

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
     [SerializeField] private float speed = 8f;
+    private bool isWalking;
     private void Update() {
         Vector2 inputVector = new Vector2(0, 0);
 
@@ -24,7 +25,14 @@ public class Player : MonoBehaviour {
 
         Vector3 movementDirection = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += movementDirection * speed * Time.deltaTime;
+
+        isWalking = movementDirection != Vector3.zero;
+
         float rotateSpeed = 10f;
         transform.forward = (Vector3.Slerp(transform.forward, movementDirection, Time.deltaTime * rotateSpeed));
+    }
+
+    public bool IsWalking() {
+        return isWalking;
     }
 }
