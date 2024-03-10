b49d52bdd11fb533ad6b5991c376455ec6a2c847
Andrey Sychev <js@asychev.me>
2024-03-10 09:08:51 +0400

    move input logic to separate class

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,24 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
     [SerializeField] private float speed = 8f;
+    [SerializeField] private GameInput gameInput;
+
     private bool isWalking;
     private void Update() {
-        Vector2 inputVector = new Vector2(0, 0);
-
-        if (Input.GetKey(KeyCode.W)) {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S)) {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A)) {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D)) {
-            inputVector.x = +1;
-        }
-
-        inputVector = inputVector.normalized;
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 movementDirection = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += movementDirection * speed * Time.deltaTime;
